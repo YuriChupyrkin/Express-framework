3d2ab657ce290625f2f65caa7cce92407db52168
Yuri Chupyrkin <yuri.chupyrkin@gmail.com>
2018-03-10 20:02:41 +0300

    Add error handler

--- routeConfig.js
+++ routeConfig.js
@@ -10,10 +10,30 @@ module.exports = class RouteConfig {
   };
 
   registerGetRoute(url, handler) {
-    this._app.get(url, handler);
+    this._app.get(url, function () {
+      try {
+        handler(...arguments);
+      } catch (error) {
+        let response = arguments[1];
+        response.status(500);
+        response.send({
+          errorMessage: error.message
+        });
+      }
+    });
   }
 
   registerPostRoute(url, handler) {
-    this._app.post(url, handler);
+    this._app.post(url, function () {
+      try {
+        handler(...arguments);
+      } catch (error) {
+        let response = arguments[1];
+        response.status(500);
+        response.send({
+          errorMessage: error.message
+        });
+      }
+    });
   }
 };
